5f03c6deb4ee9efb29e50434663bb5366f15e566
ethanl267 <ethanlesch8@gmail.com>
2025-05-12 06:15:55 +0200

    made a while loop for each move using counter method

--- TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
+++ TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
@@ -21,26 +21,46 @@ namespace TicTacToeSubmissionConole
         {
 
             // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
-
+            
             Console.SetCursorPosition(2, 19);
 
             Console.Write("Player X");
 
             Console.SetCursorPosition(2, 20);
 
-            Console.Write("Please Enter Row: ");
-            var row = Console.ReadLine();
+            Console.Write("player Y");
+            int counter = 21;
 
-            Console.SetCursorPosition(2, 22);
-
-
-            Console.Write("Please Enter Column: ");
-            var column = Console.ReadLine();
+            Console.SetCursorPosition(2, counter);
 
 
-            // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
-            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+            
 
+            while (true)
+            {
+                
+                Console.Write("Please Enter Row: ");
+                var row = Console.ReadLine();
+
+                Console.SetCursorPosition(2, counter+1);
+
+                Console.Write("Please Enter Column: ");
+                var column = Console.ReadLine();
+
+
+                // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
+                _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);
+                Console.Write("Please Enter Row: ");
+                var rowO = Console.ReadLine();
+
+                Console.SetCursorPosition(2, counter + 1);
+
+                Console.Write("Please Enter Column: ");
+                var columnO = Console.ReadLine();
+                _boardRenderer.AddMove(int.Parse(rowO), int.Parse(columnO), PlayerEnum.O, true);
+
+                counter = counter + 2;
+            }
         }
 
     }
